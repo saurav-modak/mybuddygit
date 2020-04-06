fb8c5883572883ea1f069823879e73fd787cc43c
Saurav Modak <63024371+saurav-modak@users.noreply.github.com>
2020-04-07 05:26:22 +0530

    New message usear live search function added

--- messages.php
+++ messages.php
@@ -64,7 +64,9 @@ if(isset($_POST['post_message'])){
                 <?php
                     if($user_to == "new"){
                         echo "Select the friend you would like to message <br><br>";
-                        echo "To:<input type='text' onkeyup='getUsers(this.value, <?php echo $userLoggedIn; ?>)' name='q' placeholder='Name' autocomplete='off' id='search_text_input'>";
+                        ?>
+                        To:<input type='text' onkeyup='getUsers(this.value, "<?php echo $userLoggedIn; ?>")' name='q' placeholder='Name' autocomplete='off' id='search_text_input'></input>
+                        <?php
                         echo "<div class='results'></div>";
                     }
                     else {
