43554f8af14359981312fa2532f5bf097d078c07
Saurav Modak <63024371+saurav-modak@users.noreply.github.com>
2020-05-28 09:32:58 +0530

    Added active users and profile on requests.php

--- requests.php
+++ requests.php
@@ -1,6 +1,26 @@
 <?php
     include("includes/header.php");
 ?>
+
+<div class="left_side_bar">
+        <div class="user_details column">
+            <a href="upload.php">
+            <img src="<?php echo $user['profile_pic']; ?>">
+        </a>
+            <div class="user_details_left_right">
+                <a href="<?php echo $userLoggedIn ?>">
+                    <?php echo $user['first_name']." ".$user['last_name'];?>
+                </a>
+                <br>
+                <?php
+                    echo "Posts: ".$user['num_posts']."<br>";
+                    echo "Likes: ".$user['num_likes'];
+                ?>
+            </div>
+        </div>
+        <?php include("online_friends.php"); ?>
+    </div>
+
 <div class="main_column column" id="main_column">
     <div class="friend_requests">
     <h4>Friend Requests</h4>
